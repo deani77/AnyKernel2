e601c4ce07551e209d03ed17428d02ebc3d4fec4
Nathan Chancellor <natechancellor@gmail.com>
2016-10-05 02:31:08 -0700

    AnyKernel: Initial Angler setup
    This adds the kernel.string, device.name, and block variables for Angler
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -3,19 +3,19 @@
 
 ## AnyKernel setup
 # EDIFY properties
-kernel.string=
+kernel.string=Stock Angler Kernel
 do.devicecheck=1
 do.initd=0
 do.modules=0
 do.cleanup=1
-device.name1=
-device.name2=
+device.name1=angler
+device.name2=Angler
 device.name3=
 device.name4=
 device.name5=
 
 # shell variables
-block=;
+block=/dev/block/platform/soc.0/f9824900.sdhci/by-name/boot;
 is_slot_device=0;
 
 
